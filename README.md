a3a7f1d3bf4aa5f314a97e12889343339ca52241
T <337290074@qq.com>
2021-03-30 17:43:28 +0800

    Updated dev

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 基于 gulp 优化创建项目的步骤以及 sass + rem适配 + 热更新等等
 
+### yarn
+
+一直使用NPM，体验了下yarn
+
 ### 启动项目
 
 yarn install 
@@ -10,12 +14,14 @@ yarn install
 
 
 <!-- √ -->
-- [ √ 默认MR模板](#template)
-- [ √ Sass 全局样式](#sass)
-- [ rem 适配方案](#rem)
-- [ √ ES6 转换压缩](#proxy)
-- [ 配置 proxy 跨域](#proxy)
-- [ √ 热更新 ](#hotupdate)
+- [ √ 默认MR模板](#)
+- [ Autoprefixer](#)
+- [ √ Sass 全局样式](#)
+- [ rem 适配方案](#)
+- [ √ ES6 转换压缩](#)
+- [ 配置 proxy 跨域](#)
+- [ √ 热更新 ](#)
+- [ ... ](#)
 <!-- - [ 添加 IE 兼容 ](#ie) -->
 <!-- ### <span id="template">✅ 默认MR模板 </span> -->
 <!-- - [√ 添加 IE 兼容 ](#ie) -->
--- gulpfile.js
+++ gulpfile.js
@@ -3,81 +3,124 @@ const del = require("del");//delete file
 const plugins = require('gulp-load-plugins')();//gulp plugins
 const concat = require('gulp-concat');//Connection file
 const babel = require('gulp-babel');//es6 => es5
-const sass = require('gulp-sass');
-/**
-* options {String or Object or Function} 这里以{Object}为例
-* {
-* 	dirname: "js",		文件路径
-* 	basename: "main",	文件名
-* 	prefix: "",		文件名前缀
-* 	suffix: ".min",	文件名后缀
-* 	extname: ".js"		文件扩展名
-* }
-*/
+const sass = require('gulp-sass');//sass => css
+const cssmin = require('gulp-clean-css');// minifiy css
+const autoFixer = require('gulp-autoprefixer');// minifiy css
+const gulpif = require('gulp-if') // 条件判断
+const uglify = require('gulp-uglify') // js压缩
+const htmlmin = require('gulp-htmlmin') // html压缩
+const imagemin = require('gulp-imagemin') // 图片压缩
+
 const rename = require('gulp-rename');
 const minimist = require('minimist');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
-var buildBasePath = 'build/';//构建输出的目录
-var projectBasePath = 'project/';//构建输出的目录
 const argv = minimist(process.argv.slice(2));
-const path = argv.name != undefined ? argv.name:"";//gulp init --name newProject
-const dist = 'dist';
+const path = argv.name != undefined ? argv.name:"";//gulp init --  newProject
+const dist = 'dist/';
 const basePath = {
-  cssDist:"dist/css",
-  cssDev:"dev/css",
-  jsDist: dist + '/js',
-  jsDev: 'dev/js',
-  imgDist: dist + '/images',
+  build:'build/',// build path
+  project:'project/',//project path
+  template:'template/'//template path
+}
+let env = 'dev'//default env
+function set_env(type) {
+    env = process.env.NODE_ENV = type || 'dev';
 }
 
 // live server
 function browser(cb){
   browserSync.init({
     server: {
-        baseDir: 'project/'+path,
+        baseDir: dist+path,
+        // baseDir: './',
         directory: true,
     },
     port: 3031
   });
-  filesWatch();
+  cb();//在task中，操作完成时，必须要通过cb()或者return的方式来告知gulp此任务已完成
+}
+
+//hot update 
+function filesWatch(cb){
+  watch(basePath.project+'**/*.html').on('change',htmlHandler); 
+  watch(basePath.project+'**/*.scss').on('change',sassHandler);
+  watch(basePath.project+'**/*.js').on('change', babelHandler); 
+  watch(basePath.project+'**/*.css').on('change', function(){
+    autoFixer();
+    cssHandler(cb);
+  }); 
+  watch([dist+'**/*']).on('change', reload); 
   cb();
 }
 
-//hot update
-function filesWatch(){
-  // watch('project/**/*.js').on('change',sassHandler);
-  watch('project/**/*.scss').on('change',sassHandler);
-  watch(['project/**/*.html','project/**/*.css','project/**/*.js']).on('change', reload); 
-  
+
+//HTML
+function htmlHandler(cb){
+  src(basePath.project+'**/*.html')  
+    .pipe(dest(dist)); 
+  cb();
 }
 
-//ES5 
-
+//JS
 function babelHandler(cb){
-  src('project/**/*.js')  
+  src(basePath.project+'**/*.js')  
     .pipe(babel())
-    .pipe(dest(basePath.jsDist)); 
-
+    .pipe(dest(dist)); 
   cb();
 }
 
-// SCSS
-function sassHandler(){
-  src('project/**/*.scss')  
+//CSS
+function cssHandler(cb){
+  src(basePath.project+'**/*.css')  
+    .pipe(dest(dist)); 
+  cb();
+}
+
+// SASS
+function sassHandler(cb){
+  src(basePath.project+'**/*.scss')  
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest("project/")); 
+    .pipe(dest(dist)); 
+  cb();
 }
 
 // init project
 function init(cb) {
-  del([projectBasePath + path])
-  src('template/**/*')
-  .pipe(dest(projectBasePath + path));
+  del([basePath.project + path])
+  src( basePath.template+'**/*')
+  .pipe(dest(basePath.project + path));
   cb();
 };
 
+//npm run dev --name TestProject
+function devHanlder(cb) {
+  set_env('dev');
+  del([dist + path]);
+  src(basePath.project + path + '**/*')
+  .pipe(dest(dist));
+  cb();
+}
+
+function bulid(cb) {
+  set_env('bulid');
+  del(basePath.bulid);
+  src(dist + path)
+  .pipe(dest(basePath.bulid))
+  cb();
+}
+
+function delDist(cb) {
+  del([dist]);
+  dest(dist)
+  cb();
+}
+//series 顺序
+//parallel 并行
 exports.babelInit = babelHandler;
-exports.init = series(init,browser);
+exports.init = series(init,browser,filesWatch);
+exports.dev = series(devHanlder,htmlHandler,sassHandler,babelHandler,cssHandler,filesWatch,browser);
+exports.bulid = bulid;
 exports.default = browser;
+exports.delDist = delDist;
